1fcdf48897e5a18b156e019b13785b23aafe74a8
emanueljg <emanueljohnsongodin@gmail.com>
2023-05-28 00:49:58 +0200

    add navigation test

--- tests/test_basic_e2e.py
+++ tests/test_basic_e2e.py
@@ -1,7 +1,10 @@
 from pytest import fixture
-from playwright.sync_api import Page, expect
+import re
+from playwright.sync_api import Page, expect, Locator
 
 BASE_URL = 'http://127.0.0.1:5000'
+
+SELECTED_PATTERN = re.compile('selected')
 
 @fixture(scope="function", autouse=True)
 def start_page(page: Page) -> None:
@@ -9,3 +12,40 @@ def start_page(page: Page) -> None:
 
 def test_home_redirect(page: Page) -> None:
     expect(page).to_have_url(f'{BASE_URL}/home')
+
+BtnContentPair = tuple[Locator, Locator]
+def _test_first_selected_second_not(selected: BtnContentPair, 
+                                    not_selected: BtnContentPair) -> None:
+    for sel in selected:
+        expect(sel).to_have_class(SELECTED_PATTERN)
+    for not_sel in not_selected:
+        expect(not_sel).not_to_have_class(SELECTED_PATTERN)
+
+def test_page_nav(page: Page) -> None:
+    btns = page.locator('#contentButtons')
+    home_button = btns.locator('#homeButton')
+    members_button = btns.locator('#membersButton')
+
+    home_button_content_pair = home_button, \
+                               page.locator('#home')
+
+    members_button_content_pair = members_button, \
+                                  page.locator('#members')
+
+    pairs = home_button_content_pair, members_button_content_pair
+
+    # start off at home
+    _test_first_selected_second_not(*pairs)
+
+    # [::1] reverses an iterable.
+    # (1, 2, 3)[::-1] = (3, 2, 1)
+
+    # flip it!
+    members_button.click()
+    pairs = pairs[::-1]
+    _test_first_selected_second_not(*pairs)
+
+    # last flip
+    home_button.click()
+    pairs = pairs[::-1]
+    _test_first_selected_second_not(*pairs)
