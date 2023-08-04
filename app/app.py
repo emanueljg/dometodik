89a36cd03525870b15ec95cdaf995ba2af0e67bc
emanueljg <emanueljohnsongodin@gmail.com>
2023-08-04 15:10:56 +0200

    touch up running

--- app/app.py
+++ app/app.py
@@ -124,4 +124,4 @@ def content_route(content: str) -> str:
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="3000", debug=True)
+    app.run(host="127.0.0.1", port=3000, debug=True)
